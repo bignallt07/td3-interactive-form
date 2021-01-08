08ed8d79488549272d5cdc06b85c4ed995fca241
Tom Bignall <bignallt@gmail.com>
2021-01-07 21:05:54 -0800

    Meets Complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-// DOM captures
+/**
+ * DOM Captures 
+ * Note: These are in order of the HTML document flow
+ */
 const form = document.querySelector("form");
 
 const nameField = document.querySelector("#name");
@@ -13,78 +16,35 @@ const shirtColorsOptions = document.querySelectorAll("#shirt-colors option");
 
 const activitiesTotalDisplay = document.querySelector("#activities-cost");
 const activitiesDiv = document.querySelector("#activities");
+const activitiesBox = document.querySelector("#activities-box");
 const activitiesCheckboxes = document.querySelectorAll("#activities input");
 
 const paymentFieldset = document.querySelector(".payment-methods");
 const paymentTypes = paymentFieldset.children;
 const payWithDiv = document.querySelector("#payment");
+const payOptions = payWithDiv.children;   // To set the load screen as payment option
 
 const creditCardInputs = document.querySelectorAll("#credit-card input");
 
 
-// Variables
+/**
+ * Global Variables
+ */
 let totalCost = 0;
 let registeredActivities = 0;
 
-// 1. Set the name field to true
-nameField.focus();                      // Think about adding to line 2
 
-
-
-// 2. Job Role - This section hides the job button on page load, then listens for a change in status, then if other input is selected, show the input box
-
-// Hide text field with id of other job role so it isn't displayed on load
+/**
+ * On load page commands
+ */
+nameField.focus();  
 jobRoleButton.style.display = "none";
-// Program select "job role" to LISTEN for user changes - show hide the text field
-jobRoleOptions.addEventListener('change', (event) => {
-    if (event.target.value === "other") {
-        jobRoleButton.style.display = "block";
-    } else {
-        jobRoleButton.style.display = "none";
-    }
-});
-
-
-// 3. T-shirts - Hide color options on page load. Then listening to an event change show the 3 t-shirt colors that match the data-theme. 
-
 shirtColorsDiv.style.display = "none";
+payWithDiv.value = "credit-card";               // Review this
 
-designButton.addEventListener("change", (event) => {
-    const designOption = event.target.value;
-    for (let i = 0; i < shirtColorsOptions.length; i++) {
-        const colorOption = shirtColorsOptions[i].getAttribute("data-theme");
-        if (colorOption !== designOption) {
-            shirtColorsOptions[i].hidden = true;
-        } else {
-            shirtColorsOptions[i].hidden = false;
-        }
-    }
-    shirtColorsDiv.style.display = "block"; 
-});
+// TO DO: Credit card show be loaded on page load - and that selection ("im going to pay with")
 
-
-// Activities Section - Updating Dollar - Listen for a change in activities fieldset, then add the set amount in data cost to the running total, if selected.
-
-activitiesDiv.addEventListener("change", e => {
-    const clicked = e.target;
-    const clickedCost = clicked.getAttribute("data-cost");
-    if (clicked.checked) {
-        totalCost += +clickedCost;
-        registeredActivities++;
-    }
-    if (!clicked.checked) {
-        totalCost -= clickedCost;
-        registeredActivities--;
-    }
-    activitiesTotalDisplay.innerHTML = `Total: $${totalCost}`;
-});
-    
-
-// Payment Section
-// 1. Credit card show be loaded on page load - and that selection ("im going to pay with")
-
-
-// Set up page - Remove bitcoin and paypal, only showing credit card info
+// Remove none-credit card fields
 for (let i = 0; i < paymentTypes.length; i++) {
     if (paymentTypes[i].id === "bitcoin" || paymentTypes[i].id === "paypal") {
         paymentTypes[i].hidden = true;
@@ -93,29 +53,20 @@ for (let i = 0; i < paymentTypes.length; i++) {
     }
 }
 
-// 2. When use selects other item, it updates - removing credit card (if needed), and added others
-payWithDiv.addEventListener("change", e => {
-    const typeOfPayment = e.target.value;
-    for (let i = 0; i < paymentTypes.length; i++) {
-        if (paymentTypes[i].tagName === "LEGEND" || paymentTypes[i].className === "payment-method-box") {
-            paymentTypes[i].hidden = false;
-        }
-        else if (paymentTypes[i].id !== typeOfPayment) {
-            paymentTypes[i].hidden = true;
-        } else {
-            paymentTypes[i].hidden = false;
-        }
-    } 
-});
-
-// NOTE - Do we add this whole page to a set up function?
-
-// Form Validation
-// Helper Functions
+// Make the focus states more obvious - Add comment about TRUE
+for (let i = 0; i < activitiesCheckboxes.length; i++) {
+    const checkbox = activitiesCheckboxes[i].parentElement;
+    checkbox.addEventListener("focus", e => {
+        checkbox.classList.add("focus");
+    }, true);
+    checkbox.addEventListener("blur", e => {
+        checkbox.classList.remove("focus");
+    }, true);
+}
 
 
 /**
- * VALIDATION FUNCTIONS
+ * Functions (For Validation) - Add function comments
  */
 
 const validateName = () => {
@@ -145,6 +96,11 @@ const validateEmailAddress = () => {
 
 const validateEnoughClasses = () => {
     const registerationValidation = registeredActivities > 0;
+    if (registerationValidation) {
+        isValid(activitiesBox);
+    } else {
+        isNotValid(activitiesBox);
+    }
     return registerationValidation;
 }
 
@@ -186,6 +142,7 @@ function isValid(element) {
     let parentOfElement = element.parentElement;
     parentOfElement.classList.add("valid");
     parentOfElement.classList.remove("not-valid");
+    console.log(parentOfElement.lastElementChild);
     parentOfElement.lastElementChild.style.display = "none";
 }
 
@@ -196,11 +153,72 @@ function isNotValid(element) {
     parentOfElement.lastElementChild.style.display = "block";
 }
 
-// Note for TOM TOMORROW: Create one handler function for all 3!
 
-// Program the form element to listen for submit event 
+ /**
+  * Event Listeners
+  */
+
+
+// To display text field if "Other" job role is selected
+jobRoleOptions.addEventListener('change', (event) => {
+    if (event.target.value === "other") {
+        jobRoleButton.style.display = "block";
+    } else {
+        jobRoleButton.style.display = "none";
+    }
+});
+ 
+// Listen to the design button being clicked, display correct options
+designButton.addEventListener("change", (event) => {
+    const designOption = event.target.value;
+    for (let i = 0; i < shirtColorsOptions.length; i++) {
+        const colorOption = shirtColorsOptions[i].getAttribute("data-theme");
+        if (colorOption !== designOption) {
+            shirtColorsOptions[i].hidden = true;
+        } else {
+            shirtColorsOptions[i].hidden = false;
+        }
+    }
+    shirtColorsDiv.style.display = "block"; 
+});
+
+
+// Listens to activities to add and subtract cost of course
+activitiesDiv.addEventListener("change", e => {
+    const clicked = e.target;
+    const clickedCost = clicked.getAttribute("data-cost");
+    if (clicked.checked) {
+        totalCost += +clickedCost;
+        registeredActivities++;
+    }
+    if (!clicked.checked) {
+        totalCost -= clickedCost;
+        registeredActivities--;
+    }
+    activitiesTotalDisplay.innerHTML = `Total: $${totalCost}`;
+});
+    
+
+// Listen to change of payment type, then add remove payment sections
+payWithDiv.addEventListener("change", e => {
+    const typeOfPayment = e.target.value;
+    for (let i = 0; i < paymentTypes.length; i++) {
+        if (paymentTypes[i].tagName === "LEGEND" || paymentTypes[i].className === "payment-method-box") {
+            paymentTypes[i].hidden = false;
+        }
+        else if (paymentTypes[i].id !== typeOfPayment) {
+            paymentTypes[i].hidden = true;
+        } else {
+            paymentTypes[i].hidden = false;
+        }
+    } 
+});
+
+
+
+// Functions to call and procedure to follow when the form is submitted
 form.addEventListener("submit", e => {
-    e.preventDefault();
+    e.preventDefault();                 // REMOVE BEFORE SUBMIT
     const name = validateName();
     const email = validateEmailAddress();
     const classes = validateEnoughClasses();
@@ -223,46 +241,6 @@ form.addEventListener("submit", e => {
         e.preventDefault();
     }
 });
-
-
-// Accessibility
-
-// 1. Make the focud states of the activities more obvious 
-for (let i = 0; i < activitiesCheckboxes.length; i++) {
-    const checkbox = activitiesCheckboxes[i].parentElement;
-    checkbox.addEventListener("focus", e => {
-        checkbox.classList.add("focus");
-    }, true);
-    checkbox.addEventListener("blur", e => {
-        checkbox.classList.remove("focus");
-    }, true);
-}
-// a. Program all of the activity CHECKBOX INPUT elements to listen for the FOCUS and BLUR events
-
-// b. When focus event is detected, add .focus class to checkbox input parent element
-
-// c. When blur event is detected, remove the .focus class from the label element that possesses it. target element with class of .focus
-
-// 2. Make form validation errors obvious
-
-// a. when user tries to submit a form, if invalid...
-
-// i. add not valid class to parent of form field or section
-// ii. Remove .valid class of parent
-// iii. dispplay .hint element associcaited with the form field - last child of parent element
-
-// b. If form valid
-
-// i. Add .valid class to parent
-// ii. Remove .not-valid from parent
-// iii. Hide .hint element
-
-// Notes for accessibility
-//1. Error messages not visibile when form loads
-//2. Don't use alerts
-//3. If empty form submitted, all fields with indicators should be displayed
-
-
 
 // Notes for the end
 // Clean up code
